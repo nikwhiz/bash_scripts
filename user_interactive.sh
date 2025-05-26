f1a1da20268ab881df013acf2c5efe774addca2b
nikwhiz <nikhilbhoite.05@gmail.com>
2025-05-26 13:47:09 +0530

    user interactivce script

---
+++ user_interactive.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+read -p "Enter the username: " username
+
+
+echo "Your username is $username"
+
+echo "What is your full name?"
+
+read -p "Enter your full name: " fullname
+
+echo "Your full name is $fullname and username is $username"
