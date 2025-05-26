1457ab356fa87a589b4503eacb19627128442246
nikwhiz <nikhilbhoite.05@gmail.com>
2025-05-26 13:59:58 +0530

    arithmatic_cal.s

---
+++ arithmatic_cal.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+read -p "first number: " x
+
+read -p "second number: " y
+
+let mul=$x*$y
+let add=$x+$y
+let div=$x/$y
+let sub=$x-$y
+echo "Multiplication $x x $y = $mul"
+echo "Add $x + $y = $add"
+echo "Div $x / $y = $div"
+echo "Sub $x - $y = $sub"
